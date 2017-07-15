96cd40fba41b819db3ec6104075914dc511e5e73
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2017-07-15 17:46:57 +0200

    Deploy script

---
+++ shell/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+SCRIPTDIR=$(dirname "$0")
+PROJECTROOT="$(dirname "$SCRIPTDIR")"
+DESTINATION="$1:/var/www/enraged.pl"
+
+cd "$(realpath $PROJECTROOT)"
+[[ $? = 0 ]] || exit 1
+export CI=true
+yarn test
+[[ $? = 0 ]] || exit 1
+export CI=false
+yarn run build
+[[ $? = 0 ]] || exit 1
+ssh "$1" "find /var/www/enraged.pl/* | grep -v \".well-known\" | xargs rm -rf"
+[[ $? = 0 ]] || exit 1
+scp -rp ./build/* "$DESTINATION"
+[[ $? = 0 ]] || exit 1
