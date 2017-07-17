e20044361c9d85c1fb6b848d1dc88f465b957c05
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2017-07-17 04:48:32 +0200

    nicer "no results"

--- src/component/SkillTable/Listing/index.js
+++ src/component/SkillTable/Listing/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Skill from '../Skill';
+import bem from '../../../util/bem';
 
 const Listing = ({skills}) => {
+    const b = bem('Listing');
     return (
-        <div>
+        <div className={b()}>
             {skills.length === 0 &&
-                <h1>Nothing. You are asking about newest Java Script framework? Try something diffrent.</h1>
+                <h1 className={b('NoResults')}>Nothing. Try something diffrent. And stop asking about this new cool Java Script framework!</h1>
             }
             {skills.map((skill, index) => {
                 return (<Skill key={index} {...skill} />)
